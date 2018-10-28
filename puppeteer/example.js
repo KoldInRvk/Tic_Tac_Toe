207ba2082bcf83bfaba55d47df1b3b618c157639
Daniel Johannesson <danni.jo@outlook.com>
2018-10-28 18:20:24 +0000

    pup lagfaering

--- puppeteer/example.js
+++ puppeteer/example.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('file:///C:/Users/danni/AppData/Local/Temp/Temp1_TicTacToe_HUG.zip/TicTacToe%20%20HUG/TicTacToe.html');
-  await page.screenshot({path: 'test.png'});
+  await page.goto('https://github.com/KoldInRvk/Tic_Tac_Toe');
+  await page.screenshot({path: 'test2.png'});
 
   await browser.close();
 })();
