82274ab931573ea418a7a28bbbe5b4432f010f84
Daniel Johannesson <danni.jo@outlook.com>
2018-10-28 18:33:07 +0000

    Pup skjaskot doc

--- puppeteer/hn.js
+++ puppeteer/hn.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'hn.pdf', format: 'A4'});
+  await page.goto('https://github.com/KoldInRvk/Tic_Tac_Toe/blob/master/docs/Design_report.markdown', {waitUntil: 'networkidle2'});
+  await page.pdf({path: 'Design_Manual.pdf', format: 'A4'});
 
   await browser.close();
 })();
