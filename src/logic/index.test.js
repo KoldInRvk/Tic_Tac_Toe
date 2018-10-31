c39c9a89f836d0c95049914fe0f200fe0d553160
kiddibry <mrkrissibry@gmail.com>
2018-10-31 13:39:11 +0000

    test skrár

--- src/logic/index.test.js
+++ src/logic/index.test.js
@@ -1,8 +1,1 @@
-const greeting = require("./index");
-
-describe("Greeting", () => {
-  it("should return a greeting with the name", () => {
-    expect(greeting("Leia")).toBe("Greetings, Leia!");
-  });
-});
 
