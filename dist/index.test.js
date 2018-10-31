93f6b094f8cafcd68ca4a47073c517df7deef95b
kiddibry <mrkrissibry@gmail.com>
2018-10-31 13:33:55 +0000

    lagaði test skrár

--- dist/index.test.js
+++ dist/index.test.js
@@ -1,7 +1,1 @@
-const greeting = require("./index");
 
-describe("Greeting", () => {
-  it("should return a greeting with the name", () => {
-    expect(greeting("Leia")).toBe("Greetings, Leia!");
-  });
-});
